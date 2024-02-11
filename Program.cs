bb9da1609ae05447d5da3a7fc0c8308fdca8af69
Dhanush <dhanushdybala10@gmail.com>
2024-02-11 15:24:30 +0530

    [Dhanush] Add: Creating a file to implement FILE-IO

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+            
